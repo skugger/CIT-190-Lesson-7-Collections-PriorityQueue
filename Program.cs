ff6df4c6596b136030144cbf39ddc73c34c8c381
chris <chris@DESKTOP-KBSPUR5>
2024-03-29 21:01:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CIT_190_Lesson_7_Collections_PriorityQueue
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            PriorityQueue<string, int> bookTitles = new();
+
+            {
+
+                bookTitles.Enqueue("The Positronic Man", 2);
+                bookTitles.Enqueue("The Caves of Steel", 4);
+                bookTitles.Enqueue("The Naked Sun", 3);
+                bookTitles.Enqueue("The Robots of Dawn", 5);
+                bookTitles.Enqueue("Pebble in the Sky", 1);
+
+                string firstTitle = bookTitles.Peek();
+                Console.WriteLine("The highest priority book title is " + firstTitle);
+
+                Console.WriteLine();
+                Console.WriteLine("Do you want to delete it? (y/n)");
+                string choice = Console.ReadLine();
+
+                while (choice == "y" && bookTitles.Count > 1)
+                {
+                    bookTitles.Dequeue();
+                    firstTitle = bookTitles.Peek();
+                    Console.WriteLine("The highest priority book title is " + firstTitle);
+                    Console.WriteLine("Do you want to delete it? (y/n)");
+                    choice = Console.ReadLine();
+                }
+                Console.WriteLine();
+                
+                if (bookTitles.Count <= 0) 
+                {
+                    Console.WriteLine("All titles have been deleted.");
+                }
+            }
+        }
+    }
+}
